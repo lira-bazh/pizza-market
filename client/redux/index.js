48bc8c9fb4013368af11e4f36cf0e420dc4ba2d7
lira-lantan <79859948912@ya.ru>
2022-03-19 19:46:49 +0300

    исправлена ошибка отладчика redux

--- client/redux/index.js
+++ client/redux/index.js
@@ -5,8 +5,9 @@ const initialState = {};
 
 let store = createStore(
   rootReducer(),
-  initialState + window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
+  initialState
+  // initialState + window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  //   window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
